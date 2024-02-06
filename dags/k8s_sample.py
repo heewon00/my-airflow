ab50fcc26bd519d13d5f21067479bbbf6e2d5832
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2024-02-06 12:53:12 +0900

    Update k8s_sample.py

--- dags/k8s_sample.py
+++ dags/k8s_sample.py
@@ -28,9 +28,9 @@ with DAG(
         task_id="run_python_script",
         name="run_python_script",
         in_cluster=False,
-        cluster_context='k3s-test',
+        #cluster_context='k3s-test',
         config_file='/bitnami/airflow/kube-config/config_yujeong',
-        namespace="default",
+        namespace="airflow",
         image="python:3.10-slim",  # 이 이미지에 필요한 파이썬 스크립트와 의존성이 포함되어 있어야 합니다.
         is_delete_operator_pod=True,
         cmds=["python", "-c"],
